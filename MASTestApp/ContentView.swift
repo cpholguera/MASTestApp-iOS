0bc7414c5aabdb73e2cb0633bef58b762464e64a
Carlos Holguera <perezholguera@gmail.com>
2024-04-28 16:28:04 +0200

    Initial Commit

---
+++ MASTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MASTestApp
+//
+//  Created by Charlie on 28.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MASTestApp/MASTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MASTestAppApp.swift
+//  MASTestApp
+//
+//  Created by Charlie on 28.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MASTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
